58858afe10938ae4fc49ac407e5bee24af7d6112
Ririto Ninigaya(Ryo Tabuki's Creator) <riritoxxl23@outlook.com>
2024-11-03 13:39:09 +0200

    Update main.cpp

--- SteamworksSDKInstallerThirdParty/SteamworksSDK-AutoConfiguration/main.cpp
+++ SteamworksSDKInstallerThirdParty/SteamworksSDK-AutoConfiguration/main.cpp
@@ -6,16 +6,16 @@ namespace fs = std::filesystem;
 
 int main() 
 {
-	wstring zk = fs::current_path();
-	wstring steamworks_sdkinc = zk + L"\\public\\steam";
-	wstring steamapi_x86 = zk + L"\\redistributable_bin";
-	wstring steamapi_x64 = zk + L"\\redistributable_bin\\win64";
-	wstring steamworks_public = L"set STEAMWORKS_SDK_INCLUDES=" + steamworks_sdkinc;
-	wstring steam_apix86_cmd = L"set STEAMAPI_X86LIBRARY=" + steamapi_x86;
-	wstring steam_api64_cmd = L"set STEAMAPI_X64LIBRARY=" + steamapi_x64;
-	const char* lk = (const char*)steamworks_public.c_str();
-	const char* xz = (const char*)steam_apix86_cmd.c_str();
-	const char* zzk = (const char*)steam_api64_cmd.c_str();
+	string zk = fs::current_path().string();
+	string steamworks_sdkinc = zk + "\\public\\steam";
+	string steamapi_x86 = zk + "\\redistributable_bin";
+	string steamapi_x64 = zk + "\\redistributable_bin\\win64";
+	string steamworks_public = "set STEAMWORKS_SDK_INCLUDES=" + steamworks_sdkinc;
+	string steam_apix86_cmd = "set STEAMAPI_X86LIBRARY=" + steamapi_x86;
+	string steam_api64_cmd = "set STEAMAPI_X64LIBRARY=" + steamapi_x64;
+	const char* lk = steamworks_public.c_str();
+	const char* xz = steam_apix86_cmd.c_str();
+	const char* zzk = steam_api64_cmd.c_str();
 	SetConsoleTitleA("SteamworksSDK-AutoConfiguration by RiritoXXL");
 	cout << "This is Auto Configuration for STEAMWORKS SDK(SOFTWARE DEVELOPMENT KIT)... \nThis Program is Created by RiritoXXL(ME)... So Enjoy to use this!!!" << endl;
 	Sleep(3000);
